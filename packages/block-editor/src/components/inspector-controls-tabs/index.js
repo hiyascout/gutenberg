e2eebc40b86a42086fdbbfb3dd19d4640f0108f7
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-10-31 13:06:23 +1000

    Remove conditional display of tabs

--- packages/block-editor/src/components/inspector-controls-tabs/index.js
+++ packages/block-editor/src/components/inspector-controls-tabs/index.js
@@ -6,57 +6,23 @@ import { TabPanel } from '@wordpress/components';
 /**
  * Internal dependencies
  */
-import useInspectorControlsTabs from './use-inspector-controls-tabs';
 import { TAB_SETTINGS, TAB_APPEARANCE } from './utils';
 import AppearanceTab from './appearance-tab';
 import SettingsTab from './settings-tab';
+
+const tabs = [ TAB_APPEARANCE, TAB_SETTINGS ];
 
 export default function InspectorControlsTabs( {
 	blockName,
 	clientId,
 	hasBlockStyles,
 } ) {
-	const hasSingleBlockSelection = !! blockName;
-	const availableTabs = useInspectorControlsTabs();
-
-	if ( ! availableTabs.length ) {
-		return null;
-	}
-
-	// If we only have a single tab to display, skip the tab panel and just
-	// render the contents.
-	if ( availableTabs.length === 1 ) {
-		if ( availableTabs[ 0 ].name === TAB_SETTINGS.name ) {
-			return (
-				<SettingsTab
-					hasSingleBlockSelection={ hasSingleBlockSelection }
-				/>
-			);
-		}
-
-		if ( availableTabs[ 0 ].name === TAB_APPEARANCE.name ) {
-			return (
-				<AppearanceTab
-					blockName={ blockName }
-					clientId={ clientId }
-					hasBlockStyles={ hasBlockStyles }
-				/>
-			);
-		}
-	}
-
-	// We have multiple tabs with contents so render complete TabPanel.
 	return (
-		<TabPanel
-			className="block-editor-block-inspector__tabs"
-			tabs={ availableTabs }
-		>
+		<TabPanel className="block-editor-block-inspector__tabs" tabs={ tabs }>
 			{ ( tab ) => {
 				if ( tab.name === TAB_SETTINGS.name ) {
 					return (
-						<SettingsTab
-							hasSingleBlockSelection={ hasSingleBlockSelection }
-						/>
+						<SettingsTab hasSingleBlockSelection={ !! blockName } />
 					);
 				}
 
