8abec3a954d5575ad5261177b9846c613026dcef
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-11-15 18:09:02 +0200

    adds page list item to enable expansion in list view

--- packages/block-library/src/page-list/edit.js
+++ packages/block-library/src/page-list/edit.js
@@ -8,20 +8,19 @@ import classnames from 'classnames';
  */
 import {
 	BlockControls,
+	InnerBlocks,
 	useBlockProps,
 	getColorClassName,
 } from '@wordpress/block-editor';
 import { ToolbarButton, Spinner, Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { useMemo, useState, memo } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
-import { store as coreStore, useEntityRecords } from '@wordpress/core-data';
+import { useMemo, useState } from '@wordpress/element';
+import { useEntityRecords } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
  */
 import ConvertToLinksModal from './convert-to-links-modal';
-import { ItemSubmenuIcon } from '../navigation-link/icons';
 
 // We only show the edit option when page count is <= MAX_PAGE_COUNT
 // Performance of Navigation Links is not good past this value.
@@ -51,6 +50,34 @@ export default function PageListEdit( { context, clientId } ) {
 		} ),
 		style: { ...context.style?.color },
 	} );
+
+	const makeBlockTemplate = ( parentId = 0 ) => {
+		const pages = pagesByParentId.get( parentId );
+
+		if ( ! pages?.length ) {
+			return [];
+		}
+
+		return pages.reduce( ( template, page ) => {
+			const hasChildren = pagesByParentId.has( page.id );
+			const pageProps = {
+				id: page.id,
+				label: page.title?.rendered,
+				title: page.title?.rendered,
+				link: page.url,
+				hasChildren,
+			};
+			let item = null;
+			const children = makeBlockTemplate( page.id );
+			item = [ 'core/page-list-item', pageProps, children ];
+
+			template.push( item );
+
+			return template;
+		}, [] );
+	};
+
+	const pagesTemplate = makeBlockTemplate();
 
 	const getBlockContent = () => {
 		if ( ! hasResolvedPages ) {
@@ -84,9 +111,9 @@ export default function PageListEdit( { context, clientId } ) {
 		if ( totalPages > 0 ) {
 			return (
 				<ul { ...blockProps }>
-					<PageItems
-						context={ context }
-						pagesByParentId={ pagesByParentId }
+					<InnerBlocks
+						template={ pagesTemplate }
+						templateLock="all"
 					/>
 				</ul>
 			);
@@ -112,21 +139,6 @@ export default function PageListEdit( { context, clientId } ) {
 			{ getBlockContent() }
 		</>
 	);
-}
-
-function useFrontPageId() {
-	return useSelect( ( select ) => {
-		const canReadSettings = select( coreStore ).canUser(
-			'read',
-			'settings'
-		);
-		if ( ! canReadSettings ) {
-			return undefined;
-		}
-
-		const site = select( coreStore ).getEntityRecord( 'root', 'site' );
-		return site?.show_on_front === 'page' && site?.page_on_front;
-	}, [] );
 }
 
 function usePageData() {
@@ -169,89 +181,3 @@ function usePageData() {
 		};
 	}, [ pages, hasResolvedPages ] );
 }
-
-const PageItems = memo( function PageItems( {
-	context,
-	pagesByParentId,
-	parentId = 0,
-	depth = 0,
-} ) {
-	const pages = pagesByParentId.get( parentId );
-	const frontPageId = useFrontPageId();
-
-	if ( ! pages?.length ) {
-		return [];
-	}
-
-	return pages.map( ( page ) => {
-		const hasChildren = pagesByParentId.has( page.id );
-		const isNavigationChild = 'showSubmenuIcon' in context;
-		return (
-			<li
-				key={ page.id }
-				className={ classnames( 'wp-block-pages-list__item', {
-					'has-child': hasChildren,
-					'wp-block-navigation-item': isNavigationChild,
-					'open-on-click': context.openSubmenusOnClick,
-					'open-on-hover-click':
-						! context.openSubmenusOnClick &&
-						context.showSubmenuIcon,
-					'menu-item-home': page.id === frontPageId,
-				} ) }
-			>
-				{ hasChildren && context.openSubmenusOnClick ? (
-					<>
-						<button
-							className="wp-block-navigation-item__content wp-block-navigation-submenu__toggle"
-							aria-expanded="false"
-						>
-							{ page.title?.rendered }
-						</button>
-						<span className="wp-block-page-list__submenu-icon wp-block-navigation__submenu-icon">
-							<ItemSubmenuIcon />
-						</span>
-					</>
-				) : (
-					<a
-						className={ classnames(
-							'wp-block-pages-list__item__link',
-							{
-								'wp-block-navigation-item__content':
-									isNavigationChild,
-							}
-						) }
-						href={ page.link }
-					>
-						{ page.title?.rendered }
-					</a>
-				) }
-				{ hasChildren && (
-					<>
-						{ ! context.openSubmenusOnClick &&
-							context.showSubmenuIcon && (
-								<button
-									className="wp-block-navigation-item__content wp-block-navigation-submenu__toggle wp-block-page-list__submenu-icon wp-block-navigation__submenu-icon"
-									aria-expanded="false"
-								>
-									<ItemSubmenuIcon />
-								</button>
-							) }
-						<ul
-							className={ classnames( 'submenu-container', {
-								'wp-block-navigation__submenu-container':
-									isNavigationChild,
-							} ) }
-						>
-							<PageItems
-								context={ context }
-								pagesByParentId={ pagesByParentId }
-								parentId={ page.id }
-								depth={ depth + 1 }
-							/>
-						</ul>
-					</>
-				) }
-			</li>
-		);
-	} );
-} );
